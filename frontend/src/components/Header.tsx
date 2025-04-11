bb17f5547469df3711edb175184922ee8747e2b6
oskarbayy <oskarbayy@gmail.com>
2025-04-11 12:14:50 +0200

    Added a lot of pages and header NavLinks

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,30 +4,32 @@ import '../styles/Header.css';
 function Header() {
     return (
         <header className="header">
-            <h1>Inventra</h1>
+            <NavLink to="/" className="header-title no-active">
+                <h1>Inventra</h1>
+            </NavLink>
             <div className="divider" />
             <nav>
-                <NavLink to="/" className={({ isActive }) => isActive ? 'active' : undefined}>
+                <NavLink to="/">
                     <i className="fa-solid fa-chart-line" style={{ marginRight: '8px' }}></i>
                     Dashboard
                 </NavLink>
-                <NavLink to="/stock" className={({ isActive }) => isActive ? 'active' : undefined}>
+                <NavLink to="/stock">
                     <i className="fa-solid fa-boxes-stacked" style={{ marginRight: '8px' }}></i>
                     Stock
                 </NavLink>
-                <NavLink to="/orders" className={({ isActive }) => isActive ? 'active' : undefined}>
+                <NavLink to="/orders">
                     <i className="fa-solid fa-truck-fast" style={{ marginRight: '8px' }}></i>
                     Ordrer
                 </NavLink>
-                <NavLink to="/users" className={({ isActive }) => isActive ? 'active' : undefined}>
+                <NavLink to="/users">
                     <i className="fa-solid fa-users" style={{ marginRight: '8px' }}></i>
                     Brugere
                 </NavLink>
-                <NavLink to="/settings" className={({ isActive }) => isActive ? 'active' : undefined}>
+                <NavLink to="/settings">
                     <i className="fa-solid fa-gear" style={{ marginRight: '8px' }}></i>
                     Indstillinger
                 </NavLink>
-                <NavLink to="/login" className={({ isActive }) => isActive ? 'active' : undefined}>
+                <NavLink to="/login">
                     <i className="fa-solid fa-right-to-bracket" style={{ marginRight: '8px' }}></i>
                     Login
                 </NavLink>
